82fed0021b28f1b62a8d0fde4668e86fbff5927a
Jakeliny Gracielly <jakeliny.dev@gmail.com>
2021-03-24 19:24:55 -0700

    add dotenv module

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
 const tmi = require('tmi.js');
-const token = ''
 
 const opts = {
     identity: {
-    username: 'Thasfin',
-    password: token,
+    username: 'thasfinbot',
+    password: process.env.TOKEN,
     },
     channels: [ 'jakeliny' ]
 };
@@ -13,11 +13,10 @@ function mensagemChegou(alvo, contexto, mensagem, isBot) {
     if (isBot) {
       return; 
     } 
-  
-    /* Objetivo colocar o nome do bot na mensagem*/
+
   const nomeDoComando = mensagem.trim();
     if (nomeDoComando === '!discord') {
-        client.say(opts.username, `Entra no discod da devHouse!! https://discord.gg/ubP6kkyttY`);
+        client.say(alvo, `Entra no discod da devHouse!! https://discord.gg/ubP6kkyttY`);
     } else {
     return;
     }
@@ -27,11 +26,8 @@ function mensagemChegou(alvo, contexto, mensagem, isBot) {
     console.log(`* Bot entrou no endereço ${endereco}:${porta}`);
   }
 
-// Cria um cliente tmi com  nossas opções
-const client = new tmi.client(opts);
 
-// Registra nossas funções
+const client = new tmi.client(opts);
 client.on('message', mensagemChegou);
 client.on('connected', entrouNoChatDaTwitch);
-// Connecta na Twitch:
 client.connect();
