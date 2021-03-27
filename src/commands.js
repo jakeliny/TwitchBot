ac7793f376f6fc1a5e9e5fcbb2542e298a945911
Rafael Nobre <rafaelnobrequeiroz@gmail.com>
2021-03-27 17:05:04 -0400

    Changes

---
+++ src/commands.js
@@ -0,0 +1,11 @@
+module.exports = [
+  {
+    name: '!discord',
+    message: 'Entra no discord da devHouse!! https://discord.gg/ubP6kkyttY',
+  },
+  {
+    name: '!discover',
+    message:
+      'Se inscreva no discover https://maratonadiscover.rocketseat.com.br/inscricao',
+  },
+];
