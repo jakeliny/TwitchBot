5bd39c695446d6068ed619fa06c7396ce17e7e17
Jakeliny Gracielly <jakeliny.dev@gmail.com>
2021-05-15 20:50:16 -0300

    add TODO for startup command

--- src/config.js
+++ src/config.js
@@ -4,8 +4,9 @@ module.exports = {
       password: process.env.TOKEN,
     },
     channels: ['jakeliny', 'maykbrito'],
-    startup: [
-        "/color yellowgreen",
-        "A Thasfin tá na área HeyGuys"
-    ],
+    // TODO: Rodar esse comando apenas se o streamer começou agora, se começou há muito tempo ão rodar esse comando
+    // startup: [
+    //     "/color yellowgreen",
+    //     "A Thasfin tá na área HeyGuys"
+    // ],
   };
