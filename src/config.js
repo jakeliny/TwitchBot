ab1c5c2f47d44ed6a69785c475a3210d174f3095
Jakeliny Gracielly <jakeliny.dev@gmail.com>
2021-05-15 20:54:18 -0300

    fix bugs

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ module.exports = {
     },
     channels: ['jakeliny', 'maykbrito'],
     // TODO: Rodar esse comando apenas se o streamer começou agora, se começou há muito tempo ão rodar esse comando
-    // startup: [
-    //     "/color yellowgreen",
-    //     "A Thasfin tá na área HeyGuys"
-    // ],
+    startup: [
+        // "/color yellowgreen",
+        // "A Thasfin tá na área HeyGuys"
+    ],
   };
