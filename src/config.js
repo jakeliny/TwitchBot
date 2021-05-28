474b0c17e581d96320b388da614e9984a77dae11
Jakeliny Gracielly <jakeliny.dev@gmail.com>
2021-05-27 21:46:43 -0300

    add rahmai commands

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
       username: 'thasfinbot',
       password: process.env.TOKEN,
     },
-    channels: ['jakeliny', 'maykbrito'],
+    channels: ['jakeliny', 'maykbrito', 'rahmaidev'],
     // TODO: Rodar esse comando apenas se o streamer começou agora, se começou há muito tempo ão rodar esse comando
     startup: [
         // "/color yellowgreen",
